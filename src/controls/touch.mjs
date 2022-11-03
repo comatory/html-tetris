203316a7f9caf1588cdd70535c3f6076753282b1
Ondřej Synáček <ondrej@synacek.org>
2022-11-03 21:12:40 +0100

    improve touch controls responsiveness

--- src/controls/touch.mjs
+++ src/controls/touch.mjs
@@ -24,24 +24,52 @@ import { pauseGame } from "../game/game.mjs";
  * @returns {TouchBindings} bindings object
  */
 export function touchBindingsFactory(context) {
-  function handleMoveLeft() {
-    moveLeft(context);
+  const ids = {
+    left: null,
+    right: null,
+    down: null,
+    rotateCw: null,
+    rotateCcw: null,
+  };
+
+  function handleMoveLeftPressed() {
+    ids.left = createPressCallback(() => moveLeft(context));
   }
 
-  function handleMoveRight() {
-    moveRight(context);
+  function handleMoveLeftLifted() {
+    destroyPressCallback(ids.left);
   }
 
-  function handleMoveDown() {
-    moveDown(context);
+  function handleMoveRightPressed() {
+    ids.right = createPressCallback(() => moveRight(context));
   }
 
-  function handleRotateClockwise() {
-    rotateClockWise(context);
+  function handleMoveRightLifted() {
+    destroyPressCallback(ids.right);
   }
 
-  function handleRotateCounterClockwise() {
-    rotateAntiClockWise(context);
+  function handleMoveDownPressed() {
+    ids.down = createPressCallback(() => moveDown(context));
+  }
+
+  function handleMoveDownLifted() {
+    destroyPressCallback(ids.down);
+  }
+
+  function handleRotateClockwisePressed() {
+    ids.rotateCw = createPressCallback(() => rotateClockWise(context));
+  }
+
+  function handleRotateClockwiseLifted() {
+    destroyPressCallback(ids.rotateCw);
+  }
+
+  function handleRotateCounterClockwisePressed() {
+    ids.rotateCcw = createPressCallback(() => rotateAntiClockWise(context));
+  }
+
+  function handleRotateCounterClockwiseLifted() {
+    destroyPressCallback(ids.rotateCcw);
   }
 
   function handlePause() {
@@ -49,31 +77,84 @@ export function touchBindingsFactory(context) {
   }
 
   function registerTouchBindings() {
-    getArrowLeftButton().addEventListener("click", handleMoveLeft);
-    getArrowRightButton().addEventListener("click", handleMoveRight);
-    getArrowDownButton().addEventListener("click", handleMoveDown);
-    getClockwiseButton().addEventListener("click", handleRotateClockwise);
+    getArrowLeftButton().addEventListener("mousedown", handleMoveLeftPressed);
+    getArrowLeftButton().addEventListener("mouseup", handleMoveLeftLifted);
+    getArrowRightButton().addEventListener("mousedown", handleMoveRightPressed);
+    getArrowRightButton().addEventListener("mouseup", handleMoveRightLifted);
+    getArrowDownButton().addEventListener("mousedown", handleMoveDownPressed);
+    getArrowDownButton().addEventListener("mouseup", handleMoveDownLifted);
+    getClockwiseButton().addEventListener(
+      "mousedown",
+      handleRotateClockwisePressed
+    );
+    getClockwiseButton().addEventListener(
+      "mouseup",
+      handleRotateClockwiseLifted
+    );
     getCounterClockwiseButton().addEventListener(
-      "click",
-      handleRotateCounterClockwise
+      "mousedown",
+      handleRotateCounterClockwisePressed
+    );
+    getCounterClockwiseButton().addEventListener(
+      "mouseup",
+      handleRotateCounterClockwiseLifted
     );
     getPauseButton().addEventListener("click", handlePause);
   }
 
   function unregisterTouchBindings() {
-    getArrowLeftButton().removeEventListener("click", handleMoveLeft);
-    getArrowRightButton().removeEventListener("click", handleMoveRight);
-    getArrowDownButton().removeEventListener("click", handleMoveDown);
-    getClockwiseButton().removeEventListener("click", handleRotateClockwise);
-    getCounterClockwiseButton().addEventListener(
-      "click",
-      handleRotateCounterClockwise
+    getArrowLeftButton().removeEventListener(
+      "mousedown",
+      handleMoveLeftPressed
+    );
+    getArrowLeftButton().removeEventListener("mouseup", handleMoveLeftLifted);
+    getArrowRightButton().removeEventListener(
+      "mousedown",
+      handleMoveRightPressed
+    );
+    getArrowRightButton().removeEventListener("mouseup", handleMoveRightLifted);
+    getArrowDownButton().removeEventListener(
+      "mousedown",
+      handleMoveDownPressed
+    );
+    getArrowDownButton().removeEventListener("mouseup", handleMoveDownLifted);
+    getClockwiseButton().removeEventListener(
+      "mousedown",
+      handleRotateClockwisePressed
+    );
+    getClockwiseButton().removeEventListener(
+      "mouseup",
+      handleRotateClockwiseLifted
+    );
+    getCounterClockwiseButton().removeEventListener(
+      "mousedown",
+      handleRotateCounterClockwisePressed
+    );
+    getCounterClockwiseButton().removeEventListener(
+      "mouseup",
+      handleRotateCounterClockwiseLifted
     );
     getPauseButton().removeEventListener("click", handlePause);
+
+    Object.values(ids).forEach((id) => {
+      if (!id) {
+        return;
+      }
+
+      destroyPressCallback(id);
+    });
   }
 
   return {
     registerTouchBindings,
     unregisterTouchBindings,
   };
+}
+
+function createPressCallback(cb) {
+  return setInterval(cb, 100);
+}
+
+function destroyPressCallback(id) {
+  return clearInterval(id);
 }
